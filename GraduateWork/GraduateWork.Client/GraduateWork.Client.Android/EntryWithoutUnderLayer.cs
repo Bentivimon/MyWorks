f56afc748abcfc914b742ac74969419950d36d41
Unknown <88nazar88@gmail.com>
2019-05-08 00:57:36 +0300

    redesign

---
+++ GraduateWork/GraduateWork.Client/GraduateWork.Client.Android/EntryWithoutUnderLayer.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.Content.Res;
+using Android.Graphics;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using GraduateWork.Client.Droid;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+using Color = Android.Graphics.Color;
+
+[assembly: ExportRenderer(typeof(Entry), typeof(EntryWithoutUnderLayer))]
+namespace GraduateWork.Client.Droid
+{
+    public class EntryWithoutUnderLayer : EntryRenderer
+    {
+        protected override void OnElementChanged(ElementChangedEventArgs<Entry> e)
+        {
+            base.OnElementChanged(e);
+
+            if (Control == null || e.NewElement == null) return;
+
+            if (Build.VERSION.SdkInt >= BuildVersionCodes.Lollipop)
+                Control.BackgroundTintList = ColorStateList.ValueOf(Color.Transparent);
+            else
+                Control.Background.SetColorFilter(Color.Transparent, PorterDuff.Mode.SrcAtop);
+        }
+    }
+}
